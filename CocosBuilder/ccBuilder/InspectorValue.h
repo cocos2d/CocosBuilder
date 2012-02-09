01508226b30b1fb2c418066f87228fcfad8311a6
Viktor Lidholt <vlidholt@zynga.com>
2012-02-08 19:06:51 -0800

    Added more plugin loading code and started on new experimental inspector

---
+++ CocosBuilder/ccBuilder/InspectorValue.h
@@ -0,0 +1,13 @@
+//
+//  Inspector.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 2/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Inspector : NSObject
+
+@end
---
+++ CocosBuilder/ccBuilder/PlugInNode.h
@@ -0,0 +1,13 @@
+//
+//  PlugInNode.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 2/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlugInNode : NSObject
+
+@end
