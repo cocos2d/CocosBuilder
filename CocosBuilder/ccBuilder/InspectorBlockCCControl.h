cfad4cc5a99ea20d0af44c8aa262af480e67002a
Viktor Lidholt <vlidholt@zynga.com>
2012-03-23 08:21:38 -0700

    Removed CCControlExtension

---
+++ CocosBuilder/ccBuilder/InspectorBlockCCControl.h
@@ -0,0 +1,13 @@
+//
+//  InspectorBlockCCControl.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 3/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "InspectorValue.h"
+
+@interface InspectorBlockCCControl : InspectorValue
+
+@end
