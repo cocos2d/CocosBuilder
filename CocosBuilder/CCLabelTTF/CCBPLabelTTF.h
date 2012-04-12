d7668de5d0a938f27d51a0dd9f530551e7f0222f
Viktor Lidholt <vlidholt@zynga.com>
2012-04-12 02:46:22 -0700

    Updated to latest version of cocos2d

---
+++ CocosBuilder/CCLabelTTF/CCBPLabelTTF.h
@@ -0,0 +1,13 @@
+//
+//  CCBPLabelTTF.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCLabelTTF.h"
+
+@interface CCBPLabelTTF : CCLabelTTF
+
+@end
---
+++ CocosBuilder/CCLabelTTF/CCBPLabelTTF.m
@@ -0,0 +1,13 @@
+//
+//  CCBPLabelTTF.m
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 4/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCBPLabelTTF.h"
+
+@implementation CCBPLabelTTF
+
+@end
