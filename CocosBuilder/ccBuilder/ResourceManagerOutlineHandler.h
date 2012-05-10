1a03be6457c0f894542c205f29fbe8b94bd2e080
Viktor Lidholt <vlidholt@zynga.com>
2012-05-10 06:47:42 -0700

    Moved functionality of resource manager into reusable class

---
+++ CocosBuilder/ccBuilder/ResourceManagerOutlineHandler.h
@@ -0,0 +1,13 @@
+//
+//  ResourceManagerOutlineHandler.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 5/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ResourceManagerOutlineHandler : NSObject
+
+@end
---
+++ CocosBuilder/ccBuilder/ResourceManagerOutlineHandler.m
@@ -0,0 +1,13 @@
+//
+//  ResourceManagerOutlineHandler.m
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 5/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ResourceManagerOutlineHandler.h"
+
+@implementation ResourceManagerOutlineHandler
+
+@end
