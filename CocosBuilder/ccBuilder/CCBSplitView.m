045eea593871ea18050f660fe6173d5412c0007d
Viktor Lidholt <vlidholt@zynga.com>
2012-05-31 05:22:47 -0700

    Fixed the border color of the split view

---
+++ CocosBuilder/ccBuilder/CCBSplitView.m
@@ -0,0 +1,18 @@
+//
+//  CCBSplitView.m
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 5/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CCBSplitView.h"
+
+@implementation CCBSplitView
+
+- (NSColor*) dividerColor
+{
+    return [NSColor colorWithDeviceRed:0.42 green:0.42 blue:0.42 alpha:1];
+}
+
+@end
