ac8789c4f6c936166a02303eac53deb091f97b55
Viktor Lidholt <vlidholt@zynga.com>
2012-06-28 07:10:19 -0700

    Added test case for animations

---
+++ CocosBuilder/ccBuilder/SequencerKeyframeEasing.m
@@ -0,0 +1,13 @@
+//
+//  SequencerKeyframeEasing.m
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "SequencerKeyframeEasing.h"
+
+@implementation SequencerKeyframeEasing
+
+@end
