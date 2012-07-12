a113b6ead15de321c4ace66210c9c267ba0e90c8
Viktor Lidholt <vlidholt@zynga.com>
2012-07-12 09:56:40 -0700

    Cleaned up code

--- CocosBuilder/ccBuilder/JavaScriptDocument.m
+++ CocosBuilder/ccBuilder/JavaScriptDocument.m
@@ -32,8 +32,6 @@
 {
     [super windowControllerDidLoadNib:aController];
     
-    // Add any code here that needs to be executed once the windowController has loaded the document's window.
-    
     fragaria = [[MGSFragaria alloc] init];
     
     [[NSUserDefaults standardUserDefaults] setObject:[NSNumber numberWithBool:YES] forKey:MGSPrefsAutocompleteSuggestAutomatically];	
@@ -50,7 +48,6 @@
     
     // access the NSTextView
     fragariaTextView = [fragaria objectForKey:ro_MGSFOTextView];
-    //[mJavaScriptConsoleInputTextView setString:@"var director = cc.Director.getInstance();\nvar runningScene = director.getRunningScene();\n\nvar sprite = cc.Sprite.create(\"grossini.png\");\nrunningScene.addChild(sprite);"];
     
     if (docStr)
     {
@@ -64,16 +61,7 @@
 
 - (NSData *)dataOfType:(NSString *)typeName error:(NSError **)outError
 {
-    // Insert code here to write your document to data of the specified type. If outError != NULL, ensure that you create and set an appropriate error when returning nil.
-    // You can also choose to override -fileWrapperOfType:error:, -writeToURL:ofType:error:, or -writeToURL:ofType:forSaveOperation:originalContentsURL:error: instead.
-    
     return [[fragariaTextView string] dataUsingEncoding:NSUTF8StringEncoding];
-    
-    /*
-    if (outError) {
-        *outError = [NSError errorWithDomain:NSOSStatusErrorDomain code:unimpErr userInfo:NULL];
-    }
-    return nil;*/
 }
 
 - (BOOL)readFromData:(NSData *)data ofType:(NSString *)typeName error:(NSError **)outError
@@ -95,12 +83,6 @@
     docEdited = YES;
 }
 
-/*
-- (BOOL) isDocumentEdited
-{
-    return docEdited;
-}*/
-
 - (BOOL) validateMenuItem:(NSMenuItem *)menuItem
 {
     if ([menuItem.title isEqualToString:@"Save"]) return YES;
@@ -121,14 +103,6 @@
 {
     [fragariaTextView.undoManager redo];
 }
-
-/*
-- (IBAction)close:(id)sender
-{
-    NSLog(@"close!");
-    [super close];
-}*/
-
 
 - (void) dealloc
 {
