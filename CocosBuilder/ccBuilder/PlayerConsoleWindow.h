d8511bb19c6e43daf41a6548b1749cc712f1063a
Viktor Lidholt <vlidholt@zynga.com>
2012-07-14 04:51:14 -0700

    Added CocosPlayer console window

---
+++ CocosBuilder/ccBuilder/PlayerConsoleWindow.h
@@ -0,0 +1,13 @@
+//
+//  PlayerConsoleWindow.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 7/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PlayerConsoleWindow : NSWindowController
+
+@end
---
+++ CocosBuilder/ccBuilder/PlayerConsoleWindow.m
@@ -0,0 +1,42 @@
+//
+//  PlayerConsoleWindow.m
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 7/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlayerConsoleWindow.h"
+
+@interface PlayerConsoleWindow ()
+
+@end
+
+@implementation PlayerConsoleWindow
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+- (IBAction)pressedPlay:(id)sender
+{
+}
+
+- (IBAction)pressedStop:(id)sender
+{
+}
+
+@end
