d10b7f45d6733fcfecb2091ddaeef72d6b484be5
Viktor Lidholt <vlidholt@zynga.com>
2012-08-21 07:41:17 -0700

    Updated icons

---
+++ CocosBuilder/ccBuilder/MainToolbarDelegate.h
@@ -0,0 +1,13 @@
+//
+//  MainToolbarDelegate.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 8/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MainToolbarDelegate : NSObject
+
+@end
---
+++ CocosBuilder/ccBuilder/MainToolbarDelegate.m
@@ -0,0 +1,13 @@
+//
+//  MainToolbarDelegate.m
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 8/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MainToolbarDelegate.h"
+
+@implementation MainToolbarDelegate
+
+@end
