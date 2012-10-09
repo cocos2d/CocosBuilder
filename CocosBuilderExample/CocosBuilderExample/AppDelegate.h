6bafda034db126b74c69cd8125d52dc91a1b5eb1
Viktor Lidholt <vlidholt@zynga.com>
2012-10-08 18:12:03 -0700

    Updated to latest version of cocos2d

--- CocosBuilderExample/CocosBuilderExample/AppDelegate.h
+++ CocosBuilderExample/CocosBuilderExample/AppDelegate.h
@@ -1,40 +1,28 @@
-/*
- * CocosBuilder: http://www.cocosbuilder.com
- *
- * Copyright (c) 2012 Zynga Inc.
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
+//
+//  AppDelegate.h
+//  Foo
+//
+//  Created by Viktor Lidholt on 10/8/12.
+//  Copyright Viktor Lidholt 2012. All rights reserved.
+//
 
 #import <UIKit/UIKit.h>
 #import "cocos2d.h"
 
-@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
 {
 	UIWindow *window_;
-	UINavigationController *navController_;
-
+	MyNavigationController *navController_;
+    
 	CCDirectorIOS	*director_;							// weak ref
 }
 
 @property (nonatomic, retain) UIWindow *window;
-@property (readonly) UINavigationController *navController;
+@property (readonly) MyNavigationController *navController;
 @property (readonly) CCDirectorIOS *director;
 
 @end
