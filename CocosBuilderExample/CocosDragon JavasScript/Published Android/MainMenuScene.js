7e33477fc5ee2a4be12ed338816ddc1a9bb99e64
Viktor Lidholt <vlidholt@zynga.com>
2012-11-15 16:29:39 -0800

    Moved Examples and removed old ones

--- CocosBuilderExample/CocosDragon JavasScript/Published Android/MainMenuScene.js
+++
@@ -1,7 +0,0 @@
-var MainMenuScene = function(){};
-
-MainMenuScene.prototype.onPressedPlay = function()
-{
-    var scene = cc.Reader.loadAsScene("GameScene.ccbi");
-    cc.Director.getInstance().replaceScene(scene);
-};
