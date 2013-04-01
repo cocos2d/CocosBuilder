504b449bb99512ebf9d8393410a2cea6ab1a1f6f
Viktor Lidholt <vlidholt@zynga.com>
2013-04-01 15:33:09 -0700

    Added classes for handling Debugger connection

---
+++ CocosBuilder/ccBuilder/DebuggerConnection.h
@@ -0,0 +1,13 @@
+//
+//  DebuggerConnection.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 4/1/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DebuggerConnection : NSObject
+
+@end
---
+++ CocosBuilder/ccBuilder/DebuggerConnection.m
@@ -0,0 +1,13 @@
+//
+//  DebuggerConnection.m
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 4/1/13.
+//
+//
+
+#import "DebuggerConnection.h"
+
+@implementation DebuggerConnection
+
+@end
