d140f471c09317aa2326535671f78230360041b6
Viktor Lidholt <vlidholt@zynga.com>
2013-04-08 18:35:07 -0700

    Added jshint (for syntax checking) to the project

---
+++ CocosBuilder/ccBuilder/JavaScriptSyntaxChecker.h
@@ -0,0 +1,13 @@
+//
+//  JavaScriptSyntaxChecker.h
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 4/8/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JavaScriptSyntaxChecker : NSObject
+
+@end
---
+++ CocosBuilder/ccBuilder/JavaScriptSyntaxChecker.m
@@ -0,0 +1,13 @@
+//
+//  JavaScriptSyntaxChecker.m
+//  CocosBuilder
+//
+//  Created by Viktor Lidholt on 4/8/13.
+//
+//
+
+#import "JavaScriptSyntaxChecker.h"
+
+@implementation JavaScriptSyntaxChecker
+
+@end
